676b3dc8f5925e3ed500ba40f4bc1dd2f22136ad
doinkythederp <mcclelll@icloud.com>
2021-06-15 12:12:14 -0700

    Implement socket.io

--- index.js
+++ index.js
@@ -4,11 +4,37 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.sendFile('./index.html', { root: process.cwd() });
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server: ioServer } = require('socket.io');
+const io = new ioServer(server);
+
+app.use(express.static(__dirname + '/static'));
+
+io.on('connection', socket => {
+	console.log('New Connection!');
+	socket.on('disconnect', reason => {
+		console.log('User disconnected:', reason);
+	})
 });
 
-/** @type {import('http').Server} */
-const server = app.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
 	console.log(`Listening on http://${require('os').hostname() || 'localhost'}:${process.env.PORT || 3000}`)
 });
+
+process.on('SIGINT', stop);
+
+async function stop() {
+	console.log('Stopping!');
+	await Promise.all([
+		asPromise(io.close),
+		asPromise(server.close)
+	]);
+	console.log('Done!');
+
+}
+/** @param {(callback: (err?: any, out?: any) => void) => void} func */
+function asPromise(func) {
+	return new Promise((res, rej) => void func((err, out) => void(err ? rej(err) : res(out))));
+}
